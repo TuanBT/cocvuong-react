1bfb2b21111e8d56306a8d0dd5f65f58221daa45
Tuân Bùi Tiến <bttvn.4t@gmail.com>
2023-06-23 16:20:37 +0700

    Rechange back to Test

--- src/containers/test.container.jsx
+++ src/containers/test.container.jsx
@@ -7,13 +7,6 @@ class TestContainer extends Component {
     super(props);
   }
 
-  showModal = () => {
-    $('#modalChooseMatch').removeClass('modal display-none').addClass('modal display-block');
-  };
-
-  hideModal = () => {
-    $('#modalChooseMatch').removeClass('modal display-block').addClass('modal display-none');;
-  };
 
 
   render() {
@@ -23,33 +16,7 @@ class TestContainer extends Component {
         <h2>TEST PAGE</h2>
         
 
-        <button onClick={this.showModal}>
-          Launch Form modal
-        </button>
-        <div className="modal display-none" id="modalChooseMatch" tabIndex="-" role="dialog">
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="modalLabel">Trọn trận đấu</h5>
-                  <button type="button" className="close" data-dismiss="modal" onClick={this.hideModal} aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text"><i className="fa fa-code"></i></span>
-                    </div>
-                    <input type="number" className="form-control" placeholder="Số thứ tự trận đấu" id="txtMatchChoose" />
-                  </div>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-primary" onClick={this.chooseMatch}>OK</button>
-                  <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={this.hideModal}>Cancel</button>
-                </div>
-              </div>
-            </div>
-          </div>
+        
 
           </div>
       </div>
