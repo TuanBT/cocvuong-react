3dd7e6fba20eb05569e9d643932a7c20696478fa
Tuân Bùi Tiến <bttvn.4t@gmail.com>
2023-06-26 01:44:11 +0700

    Baseline to arrange doi khang

--- src/containers/test.container.jsx
+++ src/containers/test.container.jsx
@@ -20,32 +20,71 @@ class TestContainer extends Component {
     //4      
     this.schemaFighters.push([{ match: 1, weight: 1, type: 'Bán Kết', redFighter: { name: 1, code: 1 }, blueFighter: { name: 2, code: 2 } }, { match: 2, weight: 1, type: 'Bán Kết', redFighter: { name: 3, code: 3 }, blueFighter: { name: 4, code: 4 } }, { match: 3, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 'W.2', code: '' } },]);
     //5
-    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 3, code: 3 }, blueFighter: { name: 4, code: 4 } }, { match: 2, weight: 1, type: 'Bán Kết', redFighter: { name: 1, code: 1 }, blueFighter: { name: 2, code: 2 } }, { match: 3, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 5, code: 5 } }, { match: 4, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 'W.3', code: '' } }]);
+    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 3, code: 3 }, blueFighter: { name: 4, code: 4 } }, { match: 2, weight: 1, type: 'Bán Kết', redFighter: { name: 1, code: 1 }, blueFighter: { name: 2, code: 2 } }, { match: 3, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 5, code: 5 } }, { match: 4, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.2', code: '' }, blueFighter: { name: 'W.3', code: '' } }]);
     //6
-    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 2, code: 2 }, blueFighter: { name: 3, code: 3 } }, { match: 2, weight: 1, type: 'Vòng loại', redFighter: { name: 4, code: 4 }, blueFighter: { name: 5, code: 5 } }, { match: 3, weight: 1, type: 'Bán Kết', redFighter: { name: 2, code: 2 }, blueFighter: { name: 'W.1', code: '' } }, { match: 4, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.2', code: '' }, blueFighter: { name: 6, code: 6 } }, { match: 5, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.3', code: '' }, blueFighter: { name: 'W.4', code: '' } }]);
+    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 2, code: 2 }, blueFighter: { name: 3, code: 3 } }, { match: 2, weight: 1, type: 'Vòng loại', redFighter: { name: 4, code: 4 }, blueFighter: { name: 5, code: 5 } }, { match: 3, weight: 1, type: 'Bán Kết', redFighter: { name: 1, code: 1 }, blueFighter: { name: 'W.1', code: '' } }, { match: 4, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.2', code: '' }, blueFighter: { name: 6, code: 6 } }, { match: 5, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.3', code: '' }, blueFighter: { name: 'W.4', code: '' } }]);
     //7
     this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 2, code: 2 }, blueFighter: { name: 3, code: 3 } }, { match: 2, weight: 1, type: 'Vòng loại', redFighter: { name: 4, code: 4 }, blueFighter: { name: 5, code: 5 } }, { match: 3, weight: 1, type: 'Vòng loại', redFighter: { name: 6, code: 6 }, blueFighter: { name: 7, code: 7 } }, { match: 4, weight: 1, type: 'Bán Kết', redFighter: { name: 1, code: 1 }, blueFighter: { name: 'W.1', code: '' } }, { match: 5, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.2', code: '' }, blueFighter: { name: 'W.3', code: '' } }, { match: 6, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.4', code: '' }, blueFighter: { name: 'W.5', code: '' } }]);
     //8
-    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 1, code: 1 }, blueFighter: { name: 2, code: 2 } }, { match: 2, weight: 1, type: 'Vòng loại', redFighter: { name: 3, code: 3 }, blueFighter: { name: 4, code: 4 } }, { match: 3, weight: 1, type: 'Vòng loại', redFighter: { name: 5, code: 5 }, blueFighter: { name: 6, code: 6 } }, { match: 4, weight: 1, type: 'Vòng loại', redFighter: { name: 7, code: 7 }, blueFighter: { name: 8, code: 8 } }, { match: 5, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 'W.2', code: '' } }, { match: 6, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.3', code: '' }, blueFighter: { name: 'W.4', code: '' } }, { match: 7, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.5', code: '' }, blueFighter: { name: 'W.6', code: '' } }]);
+    this.schemaFighters.push([{match:1,weight:1,type:"Vòng loại",redFighter:{name:1,code:1},blueFighter:{name:2,code:2}},{match:2,weight:1,type:"Vòng loại",redFighter:{name:3,code:3},blueFighter:{name:4,code:4}},{match:3,weight:1,type:"Vòng loại",redFighter:{name:5,code:5},blueFighter:{name:6,code:6}},{match:4,weight:1,type:"Vòng loại",redFighter:{name:7,code:7},blueFighter:{name:8,code:8}},{match:5,weight:1,type:"Bán Kết",redFighter:{name:"W.1",code:""},blueFighter:{name:"W.2",code:""}},{match:6,weight:1,type:"Bán Kết",redFighter:{name:"W.3",code:""},blueFighter:{name:"W.4",code:""}},{match:7,weight:1,type:"Chung Kết",redFighter:{name:"W.5",code:""},blueFighter:{name:"W.6",code:""}}]);
     //9
-    this.schemaFighters.push([{ match: 1, weight: 1, type: 'Vòng loại', redFighter: { name: 7, code: 7 }, blueFighter: { name: 8, code: 8 } }, { match: 2, weight: 1, type: 'Vòng loại', redFighter: { name: 1, code: 1 }, blueFighter: { name: 2, code: 2 } }, { match: 3, weight: 1, type: 'Vòng loại', redFighter: { name: 3, code: 3 }, blueFighter: { name: 4, code: 4 } }, { match: 4, weight: 1, type: 'Vòng loại', redFighter: { name: 5, code: 5 }, blueFighter: { name: 6, code: 6 } }, { match: 5, weight: 1, type: 'Vòng loại', redFighter: { name: 'W.1', code: '' }, blueFighter: { name: 9, code: 9 } }, { match: 6, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.2', code: '' }, blueFighter: { name: 'W.3', code: '' } }, { match: 7, weight: 1, type: 'Bán Kết', redFighter: { name: 'W.4', code: '' }, blueFighter: { name: 'W.5', code: '' } }, { match: 8, weight: 1, type: 'Chung Kết', redFighter: { name: 'W.6', code: '' }, blueFighter: { name: 'W.7', code: '' } }]);
+    this.schemaFighters.push([{match:1,weight:1,type:"Vòng loại",redFighter:{name:6,code:6},blueFighter:{name:7,code:7}},{match:2,weight:1,type:"Vòng loại",redFighter:{name:1,code:1},blueFighter:{name:2,code:2}},{match:3,weight:1,type:"Vòng loại",redFighter:{name:3,code:3},blueFighter:{name:4,code:4}},{match:4,weight:1,type:"Vòng loại",redFighter:{name:5,code:5},blueFighter:{name:"W.1",code:""}},{match:5,weight:1,type:"Vòng loại",redFighter:{name:8,code:8},blueFighter:{name:9,code:9}},{match:6,weight:1,type:"Bán Kết",redFighter:{name:"W.2",code:""},blueFighter:{name:"W.3",code:""}},{match:7,weight:1,type:"Bán Kết",redFighter:{name:"W.4",code:""},blueFighter:{name:"W.5",code:""}},{match:8,weight:1,type:"Chung Kết",redFighter:{name:"W.6",code:""},blueFighter:{name:"W.7",code:""}}]);
 
     this.types = ['Vòng loại', 'Bán Kết', 'Chung Kết'];
   }
 
   process = () => {
     let rawData = [
-      ['1', '55kg nam', 'VDV 1', 'Đơn vị 1'],
-      ['2', '55kg nam', 'VDV 2', 'Đơn vị 2'],
-      ['3', '55kg nam', 'VDV 3', 'Đơn vị 3'],
-      ['4', '55kg nam', 'VDV 4', 'Đơn vị 4'],
-      ['5', '55kg nam', 'VDV 5', 'Đơn vị 5'],
-      ['6', '60kg nam', 'VDV 6', 'Đơn vị 6'],
-      ['7', '60kg nam', 'VDV 7', 'Đơn vị 7'],
-      ['8', '60kg nam', 'VDV 8', 'Đơn vị 8'],
-      ['9', '60kg nam', 'VDV 9', 'Đơn vị 9'],
-      ['10', '60kg nam', 'VDV 10', 'Đơn vị 10'],
-      ['11', '60kg nam', 'VDV 11', 'Đơn vị 11'],
+      ['1', '55kg nam', 'Trần Lê Minh', 'FPT University (A)'],
+      ['2', '55kg nam', 'Hoàng Duy Anh', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['3', '55kg nam', 'Nguyễn Phúc Long', 'FPT University (A)'],
+      ['4', '60kg nam', 'Võ Minh Hiền', 'FPT University (A)'],
+      ['5', '60kg nam', 'Lê Đức Trọng', 'FPT University (A)'],
+      ['6', '50kg nữ', 'Phạm Thị Thanh Phương', 'Swinburne (Việt Nam) (A)'],
+      ['7', '55kg nam', 'Lê Đắc Lợi', 'FPT University (A)'],
+      ['8', '55kg nam', 'Phan Nguyễn Quang Khang', 'Swinburne (Việt Nam) (A)'],
+      ['9', '55kg nam', 'Nguyễn Xuân Khang', 'FPT University (A)'],
+      ['10', '55kg nam', 'Nguyễn Linh Cảnh ', 'FPT University (A)'],
+      ['11', '60kg nam', 'Hồ Viết Thuận', 'Cao đẳng FPT Polytechnic (A)'],
+      ['12', '60kg nam', 'Trương Công Tuệ Tĩnh', 'Greenwich (Việt Nam) (A)'],
+      ['13', '60kg nam', 'Nguyễn Văn Bình', 'FPT University (A)'],
+      ['14', '60kg nam', 'Nguyễn Thái Vĩnh', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['15', '65kg nam', 'Lưu Triều Lễ', 'FPT University (A)'],
+      ['16', '65kg nam', 'Nguyễn Hoàng Long', 'Swinburne (Việt Nam) (A)'],
+      ['17', '65kg nam', 'Đỗ Nguyễn Đăng Khoa', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['18', '70kg nam', 'Ngô Nguyễn Thanh Phong', 'FPT University (A)'],
+      ['19', '70kg nam', 'Dương Quý Thành', 'Swinburne (Việt Nam) (A)'],
+      ['20', '70kg nam', 'Mạc Đăng Hải', 'Greenwich (Việt Nam) (A)'],
+      ['21', '70kg nam', 'Nguyễn Quốc Đạt ', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['22', '75kg nam', 'Cao Hoàng Duy', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['23', '75kg nam', 'Lê Trần Minh Đạt', 'FPT University (A)'],
+      ['24', '45kg nữ', 'Đặng Thị Huyền ', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['25', '50kg nữ', 'Nguyễn Thanh Vân', 'FPT University (A)'],
+      ['26', '50kg nữ', 'Trần Phạm Hà My', 'Greenwich (Việt Nam) (A)'],
+      ['27', '60kg nữ', 'Võ Thị Hiền Duyên', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['28', '65kg nữ', 'Nguyễn Ngọc Minh Thy', 'Swinburne (Việt Nam) (A)'],
+      ['29', '65kg nam', 'Trần Gia Quyên', 'FPT University (A)'],
+      ['30', '75kg nam', 'Trần Nhật Anh', 'Swinburne (Việt Nam) (A)'],
+      ['31', '75kg nam', 'Nguyễn Tấn Đạt ', 'Greenwich (Việt Nam) (A)'],
+      ['32', '45kg nữ', 'Phạm Ngọc Phương Nhi', 'Swinburne (Việt Nam) (A)'],
+      ['33', '55kg nữ', 'Phạm Thanh Hiền', 'FPT University (A)'],
+      ['34', '60kg nữ', 'Trần Thị Kim Oanh ', 'FPT University (A)'],
+      ['35', '65kg nữ ', 'Đặng Nguyễn Xuân Thy', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['41', '50kg nữ', 'Nguyễn Thị Hồng Yên', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['45', '65kg nam', 'Nguyễn Công Tài', 'FPT University (A)'],
+      ['46', '65kg nam', 'Lê Trung Tiến', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['47', '65kg nam', 'Cao Trí Tưởng', 'Greenwich (Việt Nam) (A)'],
+      ['48', '70kg nam', 'Nguyễn Hoàng Thuận ', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['49', '70kg nam', 'Nguyễn Thành Nhân', 'FPT University (A)'],
+      ['50', '70kg nam', 'Hoàng Minh Tiến', 'FPT University (A)'],
+      ['51', '70kg nam', 'Bùi Vĩnh Lộc', 'FPT University (A)'],
+      ['52', '75kg nam', 'Lê Hoàng Hải ', 'Swinburne (Việt Nam) (A)'],
+      ['53', '75kg nam', 'Nguyễn Anh Minh', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['54', '45kg nữ', 'Nguyễn Huỳnh Mai Ngân', 'FPT University (A)'],
+      ['55', '50kg nữ', 'Hồ Phương Vy', 'CĐ FPT Poly (CĐ Anh Quốc) (A)'],
+      ['56', '60kg nữ', 'Lê Ngọc Đông Nghi', 'Swinburne (Việt Nam) (A)'],
+      ['57', '65kg nữ', 'Võ Bùi Hải Nguyên', 'FPT University (A)'],
+      ['58', '55kg nữ', 'Trần Thị Minh Thư', 'FPT University (A)']
     ]
 
     // Tạo đối tượng Map để gom nhóm các võ sĩ theo hạng cân
@@ -59,51 +98,35 @@ class TestContainer extends Component {
       }
     });
 
-    this.groupMatchs = [];
+    this.matchs = [];
+    let matchCount = 1;
+    this.groupMatch= [];
     for (const [key, value] of groupedData.entries()) {
-      this.groupMatchs.push(this.getschedule(value));
+      this.groupMatch = this.getschedule(value);
+      this.changeMatchNumber(this.groupMatch, matchCount);
+      
+      this.groupMatch.forEach(match => {
+        this.matchs.push(match);
+      })
+      matchCount += this.groupMatch.length;
     }
 
-    // this.changeMatchNumber(this.groupMatchs[0], 3)
-    // console.log(this.groupMatchs[0]);
+    this.tournamentArray = this.matchs.slice();
 
-    this.tournamentArray = [];
-    let matchCount = 0;
-
-    this.types.forEach(type => {
-      this.groupMatchs.forEach(matchs => {
-        for (let i = 0; i < matchs.length; i++) {
-          const match = matchs[i];
-          if (match.type === type) {
-            this.changeMatchNumber(matchs, matchCount + 1);
-            matchCount++;
-            console.log(match);
-            this.tournamentArray.push(match);
-            matchs.splice(i, 1);
-            i--;
-          }
-        }
-      })
+    // Sắp xếp mảng theo thứ tự "Vòng loại", "Bán kết", "Chung kết"
+    this.matchs.sort((a, b) => {
+      const typeOrder = { 'Vòng loại': 0, 'Bán Kết': 1, 'Chung Kết': 2 };
+      return typeOrder[a.type] - typeOrder[b.type];
     });
 
+    console.log(this.matchs);
 
-    // this.tournamentArray = [];
-    // let matchCount = 0;
-    // this.types.forEach(type => {
-    //   this.groupMatchs.forEach(matchs => {
-    //     matchs.forEach(match => {
-    //       if(match.type === type){
-    //         matchCount++;
-    //         this.changeMatchNumber(matchs, matchCount);
-    //         let matchTemp = match;
-    //         console.log(matchTemp);
-    //         this.tournamentArray.push(matchTemp);
-    //         matchs.shift();
-    //       }
-    //     })
-    //   })
-    // })
-
+    
+    for(let i=0; i< this.matchs.length;i++){
+      if(this.matchs[i].match !== this.tournamentArray[i].match){
+        this.swapRowsName(this.tournamentArray,this.tournamentArray[i].match,i + 1)
+      }
+    }
     console.log(this.tournamentArray);
   }
 
@@ -113,6 +136,7 @@ class TestContainer extends Component {
     for (let i = 0; i < schemaFighter.length; i++) {
       let match = schemaFighter[i];
       if (!isNaN(parseFloat(match.weight))) {
+        console.log(fighters);
         match.weight = fighters[match.weight - 1][1];
       }
       if (!isNaN(parseFloat(match.redFighter.name))) {
@@ -135,13 +159,13 @@ class TestContainer extends Component {
       match.match += variance;
       if (match.redFighter.name.includes('W.')) {
         let number = parseFloat(match.redFighter.name.split('.')[1]);
-        if (newMatchNumber <= number) {
+        if (newMatchNumber > number) {
           match.redFighter.name = 'W.' + (number + variance);
         }
       }
       if (match.blueFighter.name.includes('W.')) {
         let number = parseFloat(match.blueFighter.name.split('.')[1]);
-        if (newMatchNumber <= number) {
+        if (newMatchNumber > number) {
           match.blueFighter.name = 'W.' + (number + variance);
         }
       }
@@ -149,9 +173,69 @@ class TestContainer extends Component {
     return groupMatch;
   }
 
+  swapRowsName(arr, match1, match2) {
+    // Tìm vị trí của 2 row cần đổi chỗ trong mảng
+    let index1 = arr.findIndex((el) => el.match === match1);
+    let index2 = arr.findIndex((el) => el.match === match2);
+  
+    // Nếu không tìm thấy row tương ứng thì trả về mảng ban đầu
+    if (index1 === -1 || index2 === -1) {
+      return arr;
+    }
 
+    //Đổi match
+    let matchTemp = arr[index1].match;
+    arr[index1].match = arr[index2].match;
+    arr[index2].match = matchTemp;
+  
+    //Đổi row
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  
+    // //Đổi W. tương ứng
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].redFighter.name === "W." + match1) {
+        arr[i].redFighter.name = "W." + match2;
+      }
+      if (arr[i].blueFighter.name === "W." + match1) {
+        arr[i].blueFighter.name = "W." + match2;
+      }
+      if (arr[i].redFighter.name === "W." + match2) {
+        arr[i].redFighter.name = "W." + match1;
+      }
+      if (arr[i].blueFighter.name === "W." + match2) {
+        arr[i].blueFighter.name = "W." + match1;
+      }
+    }
 
-
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (arr[i].redFighter.name === "W." + match1) {
+    //     arr[i].redFighter.name = 'NameTemp';
+    //   }
+    //   if (arr[i].blueFighter.name === "W." + match1) {
+    //     arr[i].blueFighter.name = 'NameTemp';
+    //   }
+    // }
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (arr[i].redFighter.name === "W." + match2) {
+    //     arr[i].redFighter.name = "W." + match1;
+    //   }
+    //   if (arr[i].blueFighter.name === "W." + match2) {
+    //     arr[i].blueFighter.name = "W." + match1;
+    //   }
+    // }
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (arr[i].redFighter.name === 'NameTemp') {
+    //     arr[i].redFighter.name = "W." + match1;
+    //   }
+    //   if (arr[i].blueFighter.name === 'NameTemp') {
+    //     arr[i].blueFighter.name = "W." + match1;
+    //   }
+    // }
+  
+    return arr;
+  }
 
   render() {
     return (
