10bccd7792e5f49f34970ba93ce2c49c42c29472
TuanBT <bttvn.4t@gmail.com>
2024-12-12 22:05:54 +0700

    Create ENV

--- src/firebase.js
+++ src/firebase.js
@@ -2,31 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 function Firebase() {
-  //Dev
-  const firebaseConfigDev = {
-    apiKey: "AIzaSyDN_BFuEJOqHqNzQN1w1v-6hWRDIeNgf9I",
-    authDomain: "fvc-score.firebaseapp.com",
-    databaseURL: "https://fvc-score.firebaseio.com",
-    projectId: "fvc-score",
-    storageBucket: "fvc-score.appspot.com",
-    messagingSenderId: "1052570611922",
-    appId: "1:1052570611922:web:d80a668f5fe7e251d5bd0b"
-  };
-
-  //Prod
-  const firebaseConfigProd = {
-    apiKey: "AIzaSyAYhgRYFeTWBjYZhQ0qlFsx16XAq4xae24-6hWRDIeNgf9I",
-    authDomain: "cocvuong-se60824.firebaseapp.com",
-    databaseURL: "https://cocvuong-se60824-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "cocvuong-se60824",
-    storageBucket: "cocvuong-se60824.appspot.com",
-    messagingSenderId: "18642638149",
-    appId: "1:18642638149:web:1695a62cd7947249619a2c"
+  console.log("Database environment: ", process.env.REACT_APP_FIREBASE_ENV);
+  
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   // Initialize Firebase
-  // const app = initializeApp(firebaseConfigDev);
-  const app = initializeApp(firebaseConfigProd);
+  const app = initializeApp(firebaseConfig);
   // Initialize Realtime Database and get a reference to the service
   return getDatabase(app);
 }
